21723c67b9448522e0983124ed2b4ed2b1556f4c
Morrin356 <nomuproject@hotmail.com>
2024-08-17 18:58:41 +0800

    chore: added PostGIS raster extension to webodm_dev database

--- db/init.sql
+++ db/init.sql
@@ -2,3 +2,4 @@ ALTER USER postgres PASSWORD 'postgres';
 CREATE DATABASE webodm_dev;
 ALTER DATABASE webodm_dev SET postgis.gdal_enabled_drivers TO 'GTiff';
 ALTER DATABASE webodm_dev SET postgis.enable_outdb_rasters TO True;
+CREATE extension postgis_raster CASCADE;
